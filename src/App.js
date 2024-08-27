d29e74cd8d2d87d6280f358f4161abaa969787db
lamek <lamek.lo@ksu.ac.th>
2024-08-27 16:34:00 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/Button.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
 
 function App() {
+  const employees = [
+    { name: "AAA", email: "aaa@gmail.com", age: 19 },
+    { name: "BBB", email: "bbb@gmail.com", age: 20 },
+    { name: "CCC", email: "ccc@gmail.com", age: 21 }
+  ];
+
+  console.log(employees);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <ul>
+          {employees.map((em, index) => (
+            <li key={index}>
+              ชื่อพนักงาน: {em.name}  | อีเมล์: {em.email}  | อายุ : {em.age}
+            </li>
+          ))}
+        </ul>
+        
+        <h1>hello Lamek</h1>
+        <Button text="ok" />
+        <Button text="Cancel" />
+      </div>
+      <h1 className="text01">hello Lamek</h1>
+    </>
   );
 }
 
